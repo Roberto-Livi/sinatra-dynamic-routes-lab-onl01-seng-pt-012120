43c629da793b4cedbe3b57b90efddcf4bb622a81
Roberto Livi <robertomlivi@outlook.com>
2020-04-02 01:22:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     
-    @number.times {str +="#{@phrase}\n"}
+    @number.times {str += "#{@phrase}\n"}
     string
   end
   
